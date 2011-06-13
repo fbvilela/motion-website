f8dffa1c5183fee50828cc2afe8d8893a7595ebf
Fabio Vilela <fbvilela@gmail.com>
2011-06-13 19:09:30 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MotionWebsite::Application.config.secret_token = '310dd7a21481d32c9ad2ed5fd51bd4f374f7e6974e8633da2c3fba7bae5e0dd019ba6dd110ece948d396a021b9548ddeb02144933b5393f336eb66d4bfb66a79'
